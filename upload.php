53eb247db19cafb80857541c4ce1b9734c8d3cda
lisophorm <lisophorm@gmail.com>
2014-02-14 17:08:02 +0000

    recompose image in horizontal

--- upload.php
+++ upload.php
@@ -3,3 +3,14 @@ $tempFile = $_FILES['Filedata']['tmp_name'];
 $fileName = $_FILES['Filedata']['name'];
 $fileSize = $_FILES['Filedata']['size'];
 move_uploaded_file($tempFile, "uploads/" . $fileName);
+
+$src = imagecreatefromjpeg("uploads/" . $fileName);
+$dest = imagecreatetruecolor(1860, 612);
+
+imagecopy($dest,$src,0,0,0,0,620,612);
+imagecopy($dest,$src,620,0,0,614,620,612);
+imagecopy($dest,$src,1240,0,0,1228,620,612);
+
+imagejpeg($dest,"slideshow/" . $fileName,90);
+
+?>
